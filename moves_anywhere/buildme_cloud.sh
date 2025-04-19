57ccea4c528026cb4887d54c10ff18a1b513f6c4
Timothy Fraser <timothy.fraser.1@gmail.com>
2025-04-19 11:52:33 -0400

    .

--- moves_anywhere/buildme_cloud.sh
+++ moves_anywhere/buildme_cloud.sh
@@ -18,19 +18,24 @@ DOCKER_IMAGE_TAG="v2"
 
 # Get folder for MOVES Anywhere scripts
 SCRIPTS="$(pwd)/scripts"
+echo "$SCRIPTS"
 # Process for use in linux
-dos2unix "$SCRIPTS/launch.sh"
-dos2unix "$SCRIPTS/launch_runspec.sh"
-dos2unix "$SCRIPTS/launch_fuse.sh"
-dos2unix "$SCRIPTS/task_check_inputs.sh"
-dos2unix "$SCRIPTS/task_start_mysql.sh"
-dos2unix "$SCRIPTS/task_launch_moves.sh"
-dos2unix "$SCRIPTS/task_copy_logs.sh"
-dos2unix "$SCRIPTS/task_copy_runspec.sh"
-dos2unix "$SCRIPTS/task_adapt.sh"
-dos2unix "$SCRIPTS/task_importer.sh"
-dos2unix "$SCRIPTS/task_fuse.sh"
+# dos2unix "$SCRIPTS/launch.sh"
+# dos2unix "$SCRIPTS/launch_runspec.sh"
+# dos2unix "$SCRIPTS/launch_fuse.sh"
+# dos2unix "$SCRIPTS/task_check_inputs.sh"
+# dos2unix "$SCRIPTS/task_start_mysql.sh"
+# dos2unix "$SCRIPTS/task_launch_moves.sh"
+# dos2unix "$SCRIPTS/task_copy_logs.sh"
+# dos2unix "$SCRIPTS/task_copy_runspec.sh"
+# dos2unix "$SCRIPTS/task_adapt.sh"
+# dos2unix "$SCRIPTS/task_importer.sh"
+# dos2unix "$SCRIPTS/task_fuse.sh"
 
+# Process all files for use in linux
+find "$SCRIPTS" -maxdepth 1 -name "*.sh" -print0 | while IFS= read -r -d '' file; do
+  dos2unix "$file"
+done
 
 # Set working directory
 cd "$IMAGE_FOLDER"
